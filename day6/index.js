a5bc40cff1fb99a9df94388f12d3fb2313d45bb6
Michael Bodell <mbodell@gmail.com>
2021-12-05 22:00:01 -0800

    Finished part 1

--- day6/index.js
+++ day6/index.js
@@ -4,33 +4,17 @@ const Promise = require('bluebird');
 const eachLine = Promise.promisify(lineReader.eachLine);
 
 let filename = process.argv.slice(2)[0] || 'input.txt';
-let lines = [];
+let fish = [];
 
 eachLine(filename, function(line) {
-  lines.push(line.split('->').map(e=>e.split(',').map(f=>Number(f.trim()))));
+  fish = line.split(',').map(e=>Number(e.trim()));
 }).then(function(err) {
-  hvlines = lines.filter(e=>(e[0][0]===e[1][0]||e[0][1]===e[1][1]));
-  let max = hvlines.map(e=>[Math.max(e[0][0],e[1][0]),Math.max(e[0][1],e[1][1])]).reduce((e,f)=>[Math.max(e[0],f[0]),Math.max(e[1],f[1])]);
-//  console.log(max);
-//  console.log(hvlines);
-//  console.log(lines);
-  let dia = [...Array(max[0]+1)].map(x=>Array(max[1]+1).fill(0));
-  hvlines.forEach(e => {
-    if(e[0][0] === e[1][0]) {
-      let miny = Math.min(e[0][1],e[1][1]);
-      let maxy = Math.max(e[0][1],e[1][1]);
-      for(let i=miny;i<=maxy;i++) {
-	dia[i][e[0][0]]++;
-      }
-    } else {
-      let minx = Math.min(e[0][0],e[1][0]);
-      let maxx = Math.max(e[0][0],e[1][0]);
-      for(let i=minx;i<=maxx;i++) {
-	dia[e[0][1]][i]++;
-      }
-    }
-  });
-//  console.log(dia);
-  let answer = dia.map(e=>e.filter(f=>f>1).length).reduce((a,b)=>a+b);
-  console.log(answer);
+  let days = 0;
+  for(days=1;days<=80;days++) {
+    fish = fish.map(e=>--e);
+    let newFish = fish.filter(e=>e===-1).length;
+    fish = fish.concat(new Array(newFish).fill(8));
+    fish = fish.map(e=>(e===-1)?6:e);
+  }
+  console.log(fish.length);
 });
