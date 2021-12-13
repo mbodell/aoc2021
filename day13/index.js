cd433c180b3e01bb0b606baf775246827945309a
Michael Bodell <mbodell@gmail.com>
2021-12-12 22:28:56 -0800

    Solved part 1

--- day13/index.js
+++ day13/index.js
@@ -20,6 +20,41 @@ eachLine(filename, function(line) {
     folds.push(f);
   }
 }).then(function(err) {
-  console.log(points);
-  console.log(folds);
+  let maxX = -1;
+  let maxY = -1;
+  points.map(e=>(maxX = (maxX>e[0])?maxX:e[0], maxY = (maxY>e[1])?maxY:e[1]));
+  grid = new Array(maxY+1).fill(".").map(()=>new Array(maxX+1).fill("."));
+  // Place dots
+  points.map(e=>grid[e[1]][e[0]] = "#");
+  // Do folds
+  for(let i=0;i<1/*folds.length*/;i++) {
+    let newGrid = [];
+    if(folds[i][0] === "x") {
+      omx = maxX;
+      maxX = maxX/2-1;
+      newGrid = new Array(maxY+1).fill(".").map(()=> new Array(maxX+1).fill("."));
+      for(let y=0;y<=maxY;y++) {
+        for(let x=0;x<=maxX;x++) {
+          if(grid[y][x] === "#" || grid[y][omx-x] === "#") {
+            newGrid[y][x] = "#";
+          }
+        }
+      }
+    } else {
+      omy = maxY;
+      maxY = maxY/2-1;
+      newGrid = new Array(maxY+1).fill(".").map(()=> new Array(maxX+1).fill("."));
+      for(let y=0;y<=maxY;y++) {
+        for(let x=0;x<=maxX;x++) {
+          if(grid[y][x] === "#" || grid[omy-y][x] === "#") {
+            newGrid[y][x] = "#";
+          }
+        }
+      }
+    }
+    grid=newGrid;
+  }
+  // count "#"
+  let count = grid.map(e=>e.filter(f=>f==="#").length).reduce((a,b)=>a+b);
+  console.log(count);
 });
