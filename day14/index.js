1eea283fd31e6684ee1c80fe46eafb74f636642e
Michael Bodell <mbodell@gmail.com>
2021-12-13 23:44:57 -0800

    Solved part 1

--- day14/index.js
+++ day14/index.js
@@ -5,56 +5,35 @@ const eachLine = Promise.promisify(lineReader.eachLine);
 
 let filename = process.argv.slice(2)[0] || 'input.txt';
 
-let grid = [];
-let points = [];
-let folds = [];
+let template = "";
+
+let inst = [];
+
+let firstline = -1;
 
 eachLine(filename, function(line) {
-  if(line.indexOf(",")>=0) {
-    points.push(line.split(",").map(e=>Number(e)));
-  } else if (line.indexOf("=")>=0) {
-    let inst = line.split("=");
-    let f = [];
-    f[0] = inst[0].charAt(inst[0].length-1);
-    f[1] = inst[1];
-    folds.push(f);
+  if(firstline === -1) {
+    template = line;
+    firstline++;
+  } else {
+    let instruct = line.split(" -> ");
+    if(instruct.length > 1) {
+      inst[instruct[0]] = instruct[1];
+    }
   }
 }).then(function(err) {
-  let maxX = -1;
-  let maxY = -1;
-  points.map(e=>(maxX = (maxX>e[0])?maxX:e[0], maxY = (maxY>e[1])?maxY:e[1]));
-  grid = new Array(maxY+1).fill(".").map(()=>new Array(maxX+1).fill("."));
-  // Place dots
-  points.map(e=>grid[e[1]][e[0]] = "#");
-  // Do folds
-  for(let i=0;i<1/*folds.length*/;i++) {
-    let newGrid = [];
-    if(folds[i][0] === "x") {
-      omx = maxX;
-      maxX = maxX/2-1;
-      newGrid = new Array(maxY+1).fill(".").map(()=> new Array(maxX+1).fill("."));
-      for(let y=0;y<=maxY;y++) {
-        for(let x=0;x<=maxX;x++) {
-          if(grid[y][x] === "#" || grid[y][omx-x] === "#") {
-            newGrid[y][x] = "#";
-          }
-        }
-      }
-    } else {
-      omy = maxY;
-      maxY = maxY/2-1;
-      newGrid = new Array(maxY+1).fill(".").map(()=> new Array(maxX+1).fill("."));
-      for(let y=0;y<=maxY;y++) {
-        for(let x=0;x<=maxX;x++) {
-          if(grid[y][x] === "#" || grid[omy-y][x] === "#") {
-            newGrid[y][x] = "#";
-          }
-        }
-      }
+  for(let step=0;step<10;step++) {
+    let newTemp = "";
+    for(let c=0;c<template.length-1;c++) {
+      newTemp += template[c] + inst[template.substr(c,2)]; 
     }
-    grid=newGrid;
+    newTemp += template[template.length-1];
+    template = newTemp;
   }
-  // count "#"
-  let count = grid.map(e=>e.filter(f=>f==="#").length).reduce((a,b)=>a+b);
-  console.log(count);
+  // count the letters
+  let count = [...template].reduce((a,c) => (a[c] = a[c]+1||1)&&a,{});
+  let min = template.length;
+  let max = 0;
+  Object.keys(count).map(e=>(max<count[e])?max = count[e]:1&&(count[e]<min)?min=count[e]:1);
+  console.log(max-min);
 });
