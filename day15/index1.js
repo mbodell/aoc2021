04a3e6acdfd4a7e0b86887af1b18686e41a865c3
Michael Bodell <mbodell@gmail.com>
2021-12-14 21:47:30 -0800

    Solved part 2

---
+++ day15/index1.js
@@ -0,0 +1,69 @@
+const lineReader = require("line-reader");
+const Promise = require('bluebird');
+
+const eachLine = Promise.promisify(lineReader.eachLine);
+
+let filename = process.argv.slice(2)[0] || 'input.txt';
+
+let cave = [];
+
+let costs = [];
+
+let cheapest = [];
+
+eachLine(filename, function(line) {
+  cave.push(line.split("").map(e=>Number(e)));
+}).then(function(err) {
+  // make the bigger cave
+  let cL = cave.length;
+  let big = new Array(5*cL).fill(0).map(() => new Array(5*cL));
+  for(let i=0;i<cL;i++) {
+    for(let j=0;j<cL;j++) {
+      for(let x=0;x<5;x++) {
+        for(let y=0;y<5;y++) {
+          let ans = cave[i][j]+x+y;
+          if(ans>9) {
+            ans -= 9;
+          }
+          big[i+cL*x][j+cL*y] = ans;
+        }
+      }
+    }
+  }
+  cave = big;
+
+  // normal solution
+  let end = cave.length-1;
+  cheapest = new Array(end+1).fill(0).map(() => new Array(end+1));
+  costs = new Array(end*end*9).fill(0).map(() => new Array());
+  costs[0].push([0,0]);
+  while(cheapest[end][end]===undefined) {
+    let cand = [];
+    for(let i=0;i<costs.length&&cand.length === 0;i++) {
+      if(costs[i].length > 0) {
+        cand = costs[i].pop();
+        if(cheapest[cand[0]][cand[1]]===undefined) {
+          cheapest[cand[0]][cand[1]] = i;
+          // up
+          if(cand[0]>0) {
+            costs[i+cave[cand[0]-1][cand[1]]].push([cand[0]-1,cand[1]]);
+          }
+          // down
+          if(cand[0]<end) {
+            costs[i+cave[cand[0]+1][cand[1]]].push([cand[0]+1,cand[1]]);
+          }
+          // left
+          if(cand[1]>0) {
+            costs[i+cave[cand[0]][cand[1]-1]].push([cand[0],cand[1]-1]);
+          }
+          // right
+          if(cand[1]<end) {
+            costs[i+cave[cand[0]][cand[1]+1]].push([cand[0],cand[1]+1]);
+          }
+        }
+      }
+    }
+  }
+  // console.log(cheapest);
+  console.log(cheapest[end][end]);
+});
