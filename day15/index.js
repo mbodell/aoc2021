3aaaf3c30de845a4ba4f681e895e10bb10036794
Michael Bodell <mbodell@gmail.com>
2021-12-14 20:59:52 -0800

    about to start

---
+++ day15/index.js
@@ -0,0 +1,10 @@
+const lineReader = require("line-reader");
+const Promise = require('bluebird');
+
+const eachLine = Promise.promisify(lineReader.eachLine);
+
+let filename = process.argv.slice(2)[0] || 'input.txt';
+
+eachLine(filename, function(line) {
+}).then(function(err) {
+});
