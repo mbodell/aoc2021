a2a6022518ed0068575456d8d91d581daf3c8114
Michael Bodell <mbodell@gmail.com>
2021-12-21 23:49:05 -0800

    Solved part 2

--- day22/index1.js
+++ day22/index1.js
@@ -24,11 +24,11 @@ function area(b) {
 
 function noOverlap(a,b) {
   let ret = true;
-  let sizeX = Math.max(Math.min(a[1][0],b[1][0])-Math.max(a[0][0],b[0][0]),0);
-  let sizeY = Math.max(Math.min(a[1][1],b[1][1])-Math.max(a[0][1],b[0][1]),0);
-  let sizeZ = Math.max(Math.min(a[1][2],b[1][2])-Math.max(a[0][2],b[0][2]),0);
-  let size = sizeX*sizeY*sizeZ;
-  if(size === 0) {
+  let sizeX = Math.max(Math.min(a[1][0],b[1][0])-Math.max(a[0][0],b[0][0]),-1);
+  let sizeY = Math.max(Math.min(a[1][1],b[1][1])-Math.max(a[0][1],b[0][1]),-1);
+  let sizeZ = Math.max(Math.min(a[1][2],b[1][2])-Math.max(a[0][2],b[0][2]),-1);
+  let size = Math.min(sizeX,sizeY,sizeZ);
+  if(size === -1) {
     ret = true;
   } else {
     ret = false;
@@ -43,24 +43,24 @@ function explode(box,ind) {
   let tx2 = unB[1][0];
   let cutX = [];
   let a=0,b=0;
-  if(tx1<box[0][0]&&box[0][0]<tx2) {
+  if(tx1<box[0][0]&&box[0][0]<=tx2) {
     cutX.push(box[0][0]);
     a++;
   }
-  if(tx1<box[1][0]&&box[1][0]<tx2) {
+  if(tx1<=box[1][0]&&box[1][0]<tx2) {
     cutX.push(box[1][0]);
     b++;
   }
   if(cutX.length===2) {
-    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[cutX[0],unB[1][1],unB[1][2]],unB[2]]);
-    unBox.push([[cutX[1],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[cutX[0]-1,unB[1][1],unB[1][2]],unB[2]]);
+    unBox.push([[cutX[1]+1,unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
     unB = [[cutX[0],unB[0][1],unB[0][2]],[cutX[1],unB[1][1],unB[1][2]],unB[2]];
   } else if(cutX.length===1) {
     if(a===1) {
-      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[cutX[0],unB[1][1],unB[1][2]],unB[2]]);
+      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[cutX[0]-1,unB[1][1],unB[1][2]],unB[2]]);
       unB = [[cutX[0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]; 
     } else {
-      unBox.push([[cutX[0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+      unBox.push([[cutX[0]+1,unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
       unB = [[unB[0][0],unB[0][1],unB[0][2]],[cutX[0],unB[1][1],unB[1][2]],unB[2]]; 
     }
   } else {
@@ -71,24 +71,24 @@ function explode(box,ind) {
   let ty2 = unB[1][1];
   let cutY = [];
   a=0,b=0;
-  if(ty1<box[0][1]&&box[0][1]<ty2) {
+  if(ty1<box[0][1]&&box[0][1]<=ty2) {
     cutY.push(box[0][1]);
     a++;
   }
-  if(ty1<box[1][1]&&box[1][1]<ty2) {
+  if(ty1<=box[1][1]&&box[1][1]<ty2) {
     cutY.push(box[1][1]);
     b++;
   }
   if(cutY.length===2) {
-    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],cutY[0],unB[1][2]],unB[2]]);
-    unBox.push([[unB[0][0],cutY[1],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],cutY[0]-1,unB[1][2]],unB[2]]);
+    unBox.push([[unB[0][0],cutY[1]+1,unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
     unB = [[unB[0][0],cutY[0],unB[0][2]],[unB[1][0],cutY[1],unB[1][2]],unB[2]];
   } else if(cutY.length===1) {
     if(a===1) {
-      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],cutY[0],unB[1][2]],unB[2]]);
+      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],cutY[0]-1,unB[1][2]],unB[2]]);
       unB = [[unB[0][0],cutY[0],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]; 
     } else {
-      unBox.push([[unB[0][0],cutY[0],unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+      unBox.push([[unB[0][0],cutY[0]+1,unB[0][2]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
       unB = [[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],cutY[0],unB[1][2]],unB[2]]; 
     }
   } else {
@@ -99,24 +99,24 @@ function explode(box,ind) {
   let tz2 = unB[1][2];
   let cutZ = [];
   a=0,b=0;
-  if(tz1<box[0][2]&&box[0][2]<tz2) {
+  if(tz1<box[0][2]&&box[0][2]<=tz2) {
     cutZ.push(box[0][2]);
     a++;
   }
-  if(tz1<box[1][2]&&box[1][2]<tz2) {
+  if(tz1<=box[1][2]&&box[1][2]<tz2) {
     cutZ.push(box[1][2]);
     b++;
   }
   if(cutZ.length===2) {
-    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],cutZ[0]],unB[2]]);
-    unBox.push([[unB[0][0],unB[0][1],cutZ[1]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+    unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],cutZ[0]-1],unB[2]]);
+    unBox.push([[unB[0][0],unB[0][1],cutZ[1]+1],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
     unB = [[unB[0][0],unB[0][1],cutZ[0]],[unB[1][1],unB[1][1],cutZ[2]],unB[2]];
   } else if(cutZ.length===1) {
     if(a===1) {
-      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],cutZ[0]],unB[2]]);
+      unBox.push([[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],cutZ[0]-1],unB[2]]);
       unB = [[unB[0][0],unB[0][1],cutZ[0]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]; 
     } else {
-      unBox.push([[unB[0][0],unB[0][1],cutZ[0]],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
+      unBox.push([[unB[0][0],unB[0][1],cutZ[0]+1],[unB[1][0],unB[1][1],unB[1][2]],unB[2]]);
       unB = [[unB[0][0],unB[0][1],unB[0][2]],[unB[1][0],unB[1][1],cutZ[0]],unB[2]]; 
     }
   } else {
@@ -136,26 +136,19 @@ eachLine(filename, function(line) {
   for(let i=0;i<dir.length;i++) {
     rawBox.push([[x[i][0],y[i][0],z[i][0]],[x[i][1],y[i][1],z[i][1]],dir[i]]);
   }
-  console.log(rawBox);
   while(rawBox.length>0) {
     let box = rawBox.shift();
-    console.log(box);
     let unique = true;
     for(let i=0;i<unBox.length&&unique;i++) {
       unique = noOverlap(box,unBox[i]);
       if(!unique) {
-        console.log(`overlap on ${i}:`);
-        console.log(box);
         explode(box,i);
       }
     }
     if(unique && box[2]==="on") {
       unBox.push(box);
-      console.log("Found box");
-      console.log(box);
     }
   }
-  console.log(unBox.length);
   // validate no collisions
   for(let i=0;i<unBox.length;i++) {
     for(let j=i+1;j<unBox.length;j++) {
